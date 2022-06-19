a4b100a1c87d60dc5c5cad90ae0413902d512ddb
Antonia Strack <antonia.strack@mni.thm.de>
2022-06-19 15:04:33 +0200

    DONE: add '/'-route

---
+++ backend/modules/auth/index.ts
@@ -0,0 +1,22 @@
+// Boot express
+import {Application, Request, Response} from "express";
+import express from "express";
+import cors from "cors"
+import {PORT} from "../../index";
+
+
+const app: Application = express();
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// Application routing
+app.use('/', (_req: Request, res: Response) => {
+    res.status(200).sendFile("This is sse-notes!")
+});
+
+// Start server
+app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
+
