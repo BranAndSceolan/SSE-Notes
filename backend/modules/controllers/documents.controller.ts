2f2f9fd288708499297dc2382b9d2039bbc116d3
Antonia Strack <antonia.strack@mni.thm.de>
2022-06-19 15:22:42 +0200

    DONE: add dummy functions for controller

--- backend/modules/controllers/documents.controller.ts
+++ backend/modules/controllers/documents.controller.ts
@@ -1,23 +1,35 @@
 import {Request, Response} from "express";
 
-export class UserController{
+export class DocumentsController {
     constructor() {
     }
 
     public create(req: Request, res: Response): void {
-        if (req /*valid*/) {
-            /* if ( make entry into database){
-         send res with status 200 and id
+       if (req /*valid*/) {
+           /* if ( make entry into database){
+        send res with status 200 and id
+        }else{
+            send res with status 500
+           }*/
+       } else {
+           res.status(400).send("Bad Request")
+       }
+    }
+
+    public get(req: Request, res: Response): void {
+        if (req.params /*valid*/) {
+            /* if ( successfully gotten from db){
+         send res with status 200 and values as json
          }else{
-             send res with status 500
+             send res with status 404
             }*/
         } else {
             res.status(400).send("Bad Request")
         }
     }
 
-    public get(req: Request, res: Response): void {
-        if (req.params.id /*valid*/) {
+    public getList(req: Request, res: Response): void{
+        if (req.params /*valid*/) {
             /* if ( successfully gotten from db){
          send res with status 200 and values as json
          }else{
@@ -27,17 +39,7 @@ export class UserController{
             res.status(400).send("Bad Request")
         }
     }
-    public getByName(req: Request, res: Response): void {
-        if (req.params.name /*valid*/) {
-            /* if ( successfully gotten from db){
-         send res with status 200 and values as json
-         }else{
-             send res with status 404
-            }*/
-        } else {
-            res.status(400).send("Bad Request")
-        }
-    }
+
     public delete(req: Request, res: Response): void {
         if (req /*valid*/) {
             /* if ( delete from database){
---
+++ backend/modules/controllers/index.ts
@@ -0,0 +1,5 @@
+import {DocumentsController} from "./documents.controller"
+import {UserController} from "./user.controller";
+
+export const noteController = new DocumentsController()
+export const userController = new UserController()
