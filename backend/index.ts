e794c918b16aac782fff1ae8a0aeb59e9fcd41d8
Antonia Strack <antonia.strack@mni.thm.de>
2022-06-23 09:06:44 +0200

    Done: routes defined, and implemented as prototype TODO: (other issues) connect to database, implement session handling using express-session

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,32 @@
+import express from "express";
+import {Application, Request, Response} from "express";
+import helmet from "helmet";
+
 export const PORT = 8000
+
+import {
+    notesRouter,
+    authRouter
+} from "./routes/index"
+
+// Verbindung zur Datenbank herstellen
+
+// Boot express
+export const app: Application = express();
+app.use(express.json())
+app.use(helmet)
+app.use(helmet())
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// Application routing
+app.use('/documents', notesRouter)
+app.use('/user', authRouter)
+
+app.get('/', (_req: Request, res: Response) => {
+    res.status(200).send("Welcome to SSE-NOTES!")
+});
+
+// Start server
+app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
