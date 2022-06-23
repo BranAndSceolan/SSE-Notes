f57f514113647a99845998abf1f6a83c045ba9cf
Antonia Strack <antonia.strack@mni.thm.de>
2022-06-23 12:00:26 +0200

    fix documents.router.ts

--- backend/routes/documents.router.ts
+++ backend/routes/documents.router.ts
@@ -1,23 +1,29 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response} from 'express'
 import {noteController} from "../modules/controllers";
 
 export const router = express.Router({
     strict: true
 })
 
+/**
+ * Authentication Routes
+ *  notice that the login und register request don't need to be authenticated
+ *  this is to avoid login being impossible - you can't authenticate yourself before being registered and logged in
+ */
 
+// POST Routes
 router.post('/create', (req: Request, res: Response) => {
     noteController.create(req, res)
 })
 
-router.get('/:id', (req: Request, res: Response) => {
+router.post('/:id', (req: Request, res: Response) => {
     noteController.get(req, res)
 })
 
-router.get('/getList', (req: Request, res: Response)=>{
+router.post('/list', (req: Request, res: Response) => {
     noteController.getList(req, res)
 })
 
-router.delete('/delete/:id', (req: Request, res: Response)=>{
+router.delete('/delete/:id', ((req: Request, res: Response) => {
     noteController.delete(req, res)
-})
+}))
