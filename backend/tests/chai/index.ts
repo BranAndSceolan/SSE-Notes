2b6eb59c672067618ddc78858a4dde38fb23753f
BranAndSceolan <antonia.strack@mni.thm.de>
2022-07-18 14:52:44 +0200

    test push

---
+++ backend/tests/chai/index.ts
@@ -0,0 +1,17 @@
+import {app} from '../../index';
+import chai from 'chai';
+import chaiHttp from 'chai-http'
+
+chai.use(chaiHttp)
+
+// Test base route to return string
+describe('Base Route Test', () => {
+    const returnString: String = "Welcome to SSE-NOTES!"
+    it(`should return ${returnString}`, () => {
+        return chai.request(app).get('/api')
+            .then(res => {
+                chai.expect(res.text).to.equal(returnString)
+            })
+    })
+})
+
