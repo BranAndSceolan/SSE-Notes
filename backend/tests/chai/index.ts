68b48b96011681a7baabd326861c80ae2b2643ab
BranAndSceolan <antonia.strack@mni.thm.de>
2022-07-18 15:59:34 +0200

    test push 17 (use try finally)

--- backend/tests/chai/index.ts
+++ backend/tests/chai/index.ts
@@ -6,25 +6,27 @@ chai.use(chaiHttp)
 
 let testState : number = 0
 
-// Test base route to return string
-describe('Base Route Test', () => {
-    const returnString: String = "Welcome to SSE-NOTES!"
-    it(`should return ${returnString}`, () => {
-        return chai.request(app).get('/api')
-            .then(res => {
-                chai.expect(res.text).to.equal(returnString)
-                if (res.text == returnString){
-                    testState++
-                } else{
-                    testState = -1
-                }
-            })
-    })
+try {
 
-    if (testState != -1){
+// Test base route to return string
+    describe('Base Route Test', () => {
+        const returnString: String = "Welcome to SSE-NOTES!"
+        it(`should return ${returnString}`, () => {
+            return chai.request(app).get('/api')
+                .then(res => {
+                    chai.expect(res.text).to.equal(returnString)
+                    if (res.text == returnString) {
+                        testState++
+                    } else {
+                        testState = -1
+                    }
+                })
+        })
+    })
+} finally {
+    if (testState != -1) {
         process.exit(0)
     }
-})
-
+}
 
 
