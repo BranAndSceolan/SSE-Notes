1cd77b32b3c896d9de2e1a488704f83e5f582813
BranAndSceolan <antonia.strack@mni.thm.de>
2022-07-18 16:36:38 +0200

    test push 26

--- backend/tests/chai/index.ts
+++ backend/tests/chai/index.ts
@@ -2,6 +2,7 @@ import {app} from '../../index';
 import chai from 'chai';
 import chaiHttp from 'chai-http'
 import config from "config";
+import {printToConsole} from "../../modules/util/util";
 
 chai.use(chaiHttp)
 
@@ -15,9 +16,11 @@ chai.use(chaiHttp)
                 .then(res => {
                     chai.expect(res.text).to.equal(returnString)
                     testResult = (res.text == returnString)
+                    printToConsole(testResult+ " ")
                 })
         })
         if (config.get('nodb') == "true") {
+            printToConsole(testResult+" ")
             if (testResult) {
                 process.exit(0)
             } else{
