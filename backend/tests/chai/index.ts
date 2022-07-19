1cc0d39928e1f2a59bbef195906d435ef7491db0
BranAndSceolan <antonia.strack@mni.thm.de>
2022-07-19 15:56:49 +0200

    test push: user test with docker 3 all in one file

--- backend/tests/chai/index.ts
+++ backend/tests/chai/index.ts
@@ -2,8 +2,8 @@ import {app} from '../../index';
 import chai from 'chai';
 import chaiHttp from 'chai-http'
 import config from "config";
-import {registerTest} from "./register.test";
 import {printToConsole} from "../../modules/util/util";
+import crypto from "crypto";
 
 chai.use(chaiHttp)
 
@@ -22,9 +22,29 @@ chai.use(chaiHttp)
                 })
         })
 
-        it("register test", async ()=>{
-            testResult = await registerTest()
-            printToConsole(testResult+"in register test")
+        const username = crypto.randomBytes(64).toString('hex')
+        describe('User Route Tests', () => {
+
+            it(`user:register: should return 200`, async () => {
+                const res = await chai.request(app).post('/users/create').send({
+                    name:	username,
+                    password:	"picket lock singer dread",
+                })
+                chai.expect(res.status).to.equal(200)
+                printToConsole(username)
+                testResult = ( testResult && res.status == 200)
+            })
+
+            it('user:login: should return 200', async ()=>{
+                const res = await chai.request(app).post('/users/create').send({
+                    name:	username,
+                    password:	"picket lock singer dread",
+                })
+                chai.expect(res.status).to.equal(200)
+                printToConsole(res.text)
+                testResult = ( testResult && res.status == 200)
+            })
+
         })
 
         it("result for github actions", ()=> {
